621c220f572ca8b7b9cf8b902ff52cb465dc5ee7
Mike Short <mikeshort2014@gmail.com>
2022-05-15 23:15:58 -0400

    fix: data type in fuzz_target

--- boringtun/fuzz/fuzz_targets/fuzz_target_1.rs
+++ boringtun/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,9 +1,12 @@
 #![no_main]
 #[macro_use] extern crate libfuzzer_sys;
 extern crate boringtun;
+use std::os::raw::c_int;
+use std::convert::TryInto;
 
 fuzz_target!(|data: &[u8]| {
-    // if let Ok(s) = std::str::from_utf8(data) {
-        let _ = boringtun::device::poll::block_signal(i32::from(data[0]));
-    // }
+    if data.len() >= 4 {
+        let d = i32::from_ne_bytes((&data[0..4]).try_into().unwrap());
+        let _ = boringtun::device::poll::block_signal(d);
+    }
 });
