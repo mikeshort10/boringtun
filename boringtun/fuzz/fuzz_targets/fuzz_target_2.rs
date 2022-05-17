07bc649ecbf4cef68bc592dc32c6a55c0f767692
Mike Short <mikeshort2014@gmail.com>
2022-05-16 23:00:40 -0400

    refactor: fuzz_target_2

--- boringtun/fuzz/fuzz_targets/fuzz_target_2.rs
+++ boringtun/fuzz/fuzz_targets/fuzz_target_2.rs
@@ -3,9 +3,5 @@ use libfuzzer_sys::fuzz_target;
 use boringtun::crypto::Blake2s;
 
 fuzz_target!(|data: &[u8]| {
-    // let outlen: usize = 0;
-    // let mac: bool = true;
-    // let black = Blake2s::new(data, outlen, mac);
-
-    Blake2s::new_mac(data);
+    Blake2s::new_mac(&data);
 });
