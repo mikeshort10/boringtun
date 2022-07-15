d6e0e2465bb372f9ed17449c5bc7124bced9dfd1
Mike Short <mikeshort2014@gmail.com>
2022-07-14 21:53:54 -0400

    fix: expect to handle error

--- boringtun/fuzz/fuzz_targets/fuzz_target_2.rs
+++ boringtun/fuzz/fuzz_targets/fuzz_target_2.rs
@@ -4,5 +4,5 @@ use boringtun::device;
 use std::str;
 
 fuzz_target!(|data: &[u8]| {
-    device::DeviceHandle::new(str::from_utf8(&data).unwrap(), device::DeviceConfig::default()).unwrap();
+    device::DeviceHandle::new(str::from_utf8(&data).unwrap(), device::DeviceConfig::default()).expect("file not found");
 });
